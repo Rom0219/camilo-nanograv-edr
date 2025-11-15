3bac8446048a5efb7b136306ff1dff77a32781ae
Rom0219 <crobinson1902@gmail.com>
2025-11-15 14:42:21 -0500

    Update test_enterprise_extensions.py

--- scripts/test_enterprise_extensions.py
+++ scripts/test_enterprise_extensions.py
@@ -1,1 +1,6 @@
+# scripts/test_enterprise_extensions.py
+from enterprise_extensions import model_utils, models
+
+print("enterprise_extensions OK")
+print("model_utils version:", model_utils.__file__)
 
